b6ae2b76e975afe2a9c81e0d635081d740971ab7
Bakhaow <54178022+Bakhaow@users.noreply.github.com>
2020-03-16 19:45:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
- 
+let pupil = message.guild.roles.find(role => role.name === "Eleve");
 
 client.on('ready', () => {
 
@@ -12,10 +10,13 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'register') {
-
-       message.reply('registered');
-
+    if (message.content === 'eleve') {
+       if(message.member.roles.has(pupil)) {
+         message.reply('tu es déja éléve.');
+         } else {
+         message.member.addRole(pupil).catch(console.error);
+         message.reply('registered');
+       }
        }
 
 });
