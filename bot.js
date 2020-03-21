dcb11d5b6b5d1eb0ef1f9d0e483088b3016766f2
Bakhaow <54178022+Bakhaow@users.noreply.github.com>
2020-03-21 11:41:03 +0100

    Up

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    if(command === 'register') {
+    if(command === 'register' || command === 'r' || command === 'reg') {
         let nom = args[0];
         let prenom = args[1];
         let nickname = nom.charAt(0).toUpperCase() + ". " + prenom.charAt(0).toUpperCase() + prenom.slice(1);
