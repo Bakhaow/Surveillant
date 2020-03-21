cae4c0d301b3feee0bef63017c4293bd721423de
Bakhaow <54178022+Bakhaow@users.noreply.github.com>
2020-03-21 12:36:20 +0100

    Update

--- bot.js
+++ bot.js
@@ -13,14 +13,19 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    if(command === 'register' || command === 'r' || command === 'reg') {
+    if(command === 'register' || command === 'reg' || command === 'r') {
         let nom = args[0];
         let prenom = args[1];
         let nickname = nom.charAt(0).toUpperCase() + ". " + prenom.charAt(0).toUpperCase() + prenom.slice(1);
         message.member.setNickname(nickname)
         message.member.addRole("689180199224475720").catch(console.error);
         message.channel.send('Registering ' + nickname);
-    }
+    }else if(command === 'allemand' || command === 'germaniste' || command === 'ger' || command === 'g' || command === 'all' || command === 'a') {
+        message.member.addRole("689127335697317901").catch(console.error);
+    }else if(command === 'espagnol' || command === 'esp' || command === 'e' || command === 'hispaniste' || command === 'h') {
+        message.member.addRole("689126422941663251").catch(console.error);
+    }    
+    
     
 });
 
