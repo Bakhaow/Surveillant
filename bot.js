43493b745db5f0cc8c022df2248cf157ae88914b
Bakhaow <54178022+Bakhaow@users.noreply.github.com>
2020-03-21 12:46:54 +0100

    Update

--- bot.js
+++ bot.js
@@ -17,14 +17,25 @@ client.on('message', message => {
         let nom = args[0];
         let prenom = args[1];
         let nickname = nom.charAt(0).toUpperCase() + ". " + prenom.charAt(0).toUpperCase() + prenom.slice(1);
-        message.member.setNickname(nickname)
+        message.member.setNickname(nickname);
         message.member.addRole("689180199224475720").catch(console.error);
-        message.channel.send('Registering ' + nickname);
+        message.channel.send('Bienvenue ' + nickname);
     }else if(command === 'allemand' || command === 'germaniste' || command === 'ger' || command === 'g' || command === 'all' || command === 'a') {
         message.member.addRole("689127335697317901").catch(console.error);
+        message.reply('Bien joué tu es maintenant Germaniste/Allemand');
     }else if(command === 'espagnol' || command === 'esp' || command === 'e' || command === 'hispaniste' || command === 'h') {
         message.member.addRole("689126422941663251").catch(console.error);
-    }    
+        message.reply('Bien joué tu es maintenant Hispaniste/Espagnol');
+    }else if(command === 'politique' || command === 'po') {
+        message.member.addRole("689127737138610278").catch(console.error);
+        message.reply('Bien joué tu es maintenant Politicien');
+    }else if(command === 'economique' || command === 'eco') {
+        message.member.addRole("689127728796008549").catch(console.error);
+        message.reply('Bien joué tu es maintenant Economiste');
+    }else if(command === 'maths' || command === 'm') {
+        message.member.addRole("689127549418078342").catch(console.error);
+        message.reply('Bien joué tu es maintenant Mathématicien');
+    }
     
     
 });
